326af0284c64fdad9f2e2d55345786ebfdf35cec
zxcfer <zxcfer@proton.me>
2024-05-28 03:45:06 +0200

    task plotly and streamlit fix

--- goldman/goldie.py
+++ goldman/goldie.py
@@ -1,51 +1,96 @@
+import streamlit as st
 import pandas as pd
-import kaleido
 import plotly.express as px
+from plotly.subplots import make_subplots
+from datetime import datetime
+
+df = None
+
+def update_chart(company_df, sector_df):
+    # company_name = 'ACC'
+    company_name = st.session_state.company_name
+
+    specific_date = '' # "Airlines_01/02/2022"
+    
+    # Filter specific company
+    company_data = company_df[company_df["Company Name"] == company_name]
+    print(company_data)
+
+    # check if empty company_data
+    if company_data.empty:
+        st.write("No data for this company")
+        return
+
+    sector = company_data.iloc[0]["Sector"]
+    company = company_data.iloc[0]["Company Name"]
+
+    # Select Company name, Sector and 2013 to 2050 columns
+    years = [str(x) for x in range(2013, 2051)]
+
+    company_data = company_data[years]
+    company_data["Scenario name"] = company
+    company_data["Date"] = ''
+    company_data = company_data.reindex(columns=["Date", "Scenario name"] + years)
+
+    company_data['Scenario name'] = company_data['Scenario name'].astype("string")
+
+    # replace string sector in column of sector_df
+    sector_df = sector_df[(sector_df["Sector name"] == sector)]
+    sector_df = sector_df[(sector_df["Region"] == 'Global')]
+    print(sector_df)
+
+    if not sector_df.empty:
+        sector_df['Date'] = sector_df['Benchmark ID'].replace(sector+'_', '')
+        if not specific_date:
+            specific_date = sector_df.iloc[0]["Date"]
+
+        sector_df = sector_df[(sector_df["Date"] == specific_date)]
+    else:
+        sector_df['Date'] = ''
+
+    sector_columns = ['Date', 'Scenario name'] + years
+    sector_data = sector_df[sector_columns]
+
+    print(company_data)
+    print(sector_data)
+    merged_df = pd.concat([company_data, sector_data], axis=0, ignore_index=True)
+
+    merged_df = merged_df.drop(columns=['Date'])
+    merged_df = merged_df.set_index('Scenario name').unstack().unstack().reset_index(names='year').rename_axis(columns=None)
+
+    # set year column as index
+    df = merged_df.set_index('year')
+    column_names = df.columns.tolist()
+
+    print(df)
+
+    # remove company from column names
+    column_names.remove(company)
+
+    # plot the df, index as x axis, columns as y axis
+    fig = px.area(df, x=df.index, y=column_names, 
+                  color_discrete_sequence=["#FDB714", "#009CA7", "#F05023"])
+    fig2 = px.line(df, x=df.index, y=company_name)
+
+    subfig = make_subplots(specs=[[{"secondary_y": True}]])
+    subfig.add_traces(fig.data + fig2.data)
+
+    # update chart after company selector is updated
+    subfig.update_layout(title_text=company_name)
+
+    st.plotly_chart(subfig)
 
 company_name = 'ACC'
 specific_date = '' # "Airlines_01/02/2022"
 company_df = pd.read_csv("D:\data\_COMPANY.CSV")
 sector_df = pd.read_csv("D:\data\_SECTOR.CSV")
 
-# Filter specific company
-company_data = company_df[company_df["Company Name"] == company_name]
-sector = company_data.iloc[0]["Sector"]
-company = company_data.iloc[0]["Company Name"]
+# Streamlit app
+st.title('Company Climate Transition Pathway')
 
-# Select Company name, Sector and 2013 to 2050 columns
-years = [str(x) for x in range(2013, 2051)]
-
-company_data = company_data[years]
-company_data["Scenario name"] = company
-company_data["Date"] = ''
-company_data = company_data.reindex(columns=["Date", "Scenario name"] + years)
-
-company_data['Scenario name'] = company_data['Scenario name'].astype("string")
-
-# replace string sector in column of sector_df
-sector_df['Date'] = sector_df['Benchmark ID'].replace(sector+'_', '')
-if not specific_date:
-    specific_date = sector_df.iloc[0]["Date"]
-
-# filter sector_df by 'Sector name' and 'Specific date'
-sector_data = sector_df[(sector_df["Sector name"] == sector) & (sector_df["Date"] == specific_date)]
-
-sector_columns = ['Date', 'Scenario name'] + years
-sector_data = sector_df[sector_columns]
-sector_data['Scenario name'] = sector_data['Scenario name'].astype("string")
-
-print(company_data)
-print(sector_data)
-merged_df = pd.concat([company_data, sector_data], axis=0, ignore_index=True)
-print(merged_df)
-
-df = merged_df.set_index('Scenario name').unstack().unstack().reset_index(names='year').rename_axis(columns=None)
-print(df)
-
-# plot the data
-fig = px.line(company_data, y="Company Name", x=years)
-
-print("fer")
-# # Save the plot as an image
-fig.write_image("D:/tmp/out_1.png", engine="kaleido")
-print("fin")
+# Create company selector for streamlit
+company_name = st.selectbox("Company",
+                            company_df["Company Name"].unique(),
+                            on_change=update_chart,
+                            args=(company_df, sector_df),
+                            key='company_name')
--- goldman/readme.md
+++ goldman/readme.md
@@ -16,23 +16,9 @@ Use files `Company_Latest_Assessments` and `Sector_Benchmarks`
 
 3. Consider how one might filter the choice of benchmarks to relevant subsets. Layer the benchmarks in varying temperature outcomes, different colours below/between/above, as background to company
 
-4. Consider making the tool easier for others to use, more like a dashboard,  less like code/notebook etc. For example, give __drop-down selection boxes__ to choose key values
+4. Consider making the tool easier for others to use, more like a dashboard, less like code/notebook etc. For example, give __drop-down selection boxes__ to choose key values
 
-5. Step 5 is optional if time permits -  write a simple function to discover companies that are **significant outliers** compared to their sector peers, either for good or bad transition plans
+5. Step 5 is optional if time permits -  write a simple function to discover companies that are **significant outliers** compared to their sector peers.
 
 * Plotly 
 * Streamlit
-
-Thank you and good luck!
-
-Jake
-
-1. 
-
-Create a Python script to draw a chart of performance index per year of a specific company and in the same chart plot the same index of the sector. We have two CSV files, the first CSV is "COMP.CSV" and has the columns: "Sector name", "Scenario name", "2019", "2020" and "2021" columns. The second one "SECTOR.CSV" has the columns "Company Name", "Sector" and "2019", "2020" and "2021".
-Company will be specified by name in a variable.
-Imange should be stored in "D:\tmp\out.png"
-Use Pandas, Plotly.
-
-Create a streamlit dashboard in which I will show a chart created by plotly. The dashboard should have two dropdown input boxes. 
-The first input box has a large set of data, it should be filled with a list of 100 items, so it should have search. The seond drop down is much simpler and contains a list of 3 items
